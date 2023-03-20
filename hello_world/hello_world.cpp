6b66ba8770966087bbbffbb9e7564eb41d9c2ab0
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2023-03-20 14:47:01 -0400

    Fixed minor errors

--- hello_world/hello_world.cpp
+++ hello_world/hello_world.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 // #include <Kokkos_Core.hpp>
 
-int main() {
+int main(int argc, char* argv[]) {
 
     int n = 5;
     int process_id [n] = { 0, 1, 2, 3, 4, 5 };
 
     for (int i = 0; i < n; i++){
-        std::cout << "Hello from process " << i << std::endl;
+        std::cout << "Hello from process " << process_id[i] << std::endl;
     }
 }
